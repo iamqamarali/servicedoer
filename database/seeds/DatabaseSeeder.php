893eb023cd67b2c7d84f9041a3b6f806003d6df7
Qamar Ali <iamqamarali1@gmail.com>
2020-07-05 21:58:56 +0500

    asd

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
         $this->call(QuestionsSeeder::class);
         $this->call(CitiesSeeder::class);
         $this->call(LocationsSeeder::class);
-        $this->call(UsersSeeder::class);
-        $this->call(ReviewTableSeeder::class);   
+//        $this->call(UsersSeeder::class);
+  //      $this->call(ReviewTableSeeder::class);   
         $this->call(SubscriptionPackagesSeeder::class);
     }
 }
